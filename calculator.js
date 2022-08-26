d85b53c4ca40c6fa03e571a9f723c8832057a35c
NTAparad <111552067+NTAparad@users.noreply.github.com>
2022-08-26 16:46:56 +0700

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -3,7 +3,14 @@ class Calculator {
     return method.cal(number1, number2);
   }
 };
+
 let calculator = new Calculator();
 let addition = new Addition();
 let abc = calculator.calculate(1,2, addition);
 console.log(abc);
+
+class Addition {
+  cal(number1, number2) {
+    return number1 + number2;
+  }
+}
